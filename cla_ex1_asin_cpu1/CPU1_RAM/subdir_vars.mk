4512256d09ac28e73010f38f713ae27c9b6baf58
PMORAM-20 <pmoram.w@gmail.com>
2025-04-08 20:18:31 +0200

    Inicialización del proyecto dual core con ejemplos

---
+++ cla_ex1_asin_cpu1/CPU1_RAM/subdir_vars.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../2837xD_RAM_lnk_cpu1.cmd 
+
+LIB_SRCS += \
+C:/ti/c2000/C2000Ware_5_04_00_00/driverlib/f2837xd/driverlib/ccs/Debug/driverlib.lib 
+
+C_SRCS += \
+../cla_ex1_asin_cpu1.c 
+
+C_DEPS += \
+./cla_ex1_asin_cpu1.d 
+
+OBJS += \
+./cla_ex1_asin_cpu1.obj 
+
+OBJS__QUOTED += \
+"cla_ex1_asin_cpu1.obj" 
+
+C_DEPS__QUOTED += \
+"cla_ex1_asin_cpu1.d" 
+
+C_SRCS__QUOTED += \
+"../cla_ex1_asin_cpu1.c" 
+
+
